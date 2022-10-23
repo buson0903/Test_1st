3c2b674eb554b64bb8d63a351046dccbf98f8042
buson0903 <hontake0903@gmail.com>
2022-10-23 19:39:20 +0900

    Change 08/5

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,8 +3,10 @@
     <head>
         <meta charset="UTF-8">
         <title>Blog</title>
+        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        
     </head>
     <body>
         <h1 class='title'>
@@ -15,6 +17,9 @@
                 <h3>本文</h3>
                 <p class='body'>{{ $post->body}}</p>
             </div>
+        </div>
+        <div class='edit'>
+            <a href="/posts/{{ $post->id }}/edit">編集</a>
         </div>
         <div class='footer'>
             <a href="/">戻る</a>
